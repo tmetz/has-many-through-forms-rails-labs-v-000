95262307b0fdbf8c7ba22c786b7a80d51bafc9da
Tammy Metz <tammy@tammymetz.com>
2019-02-03 18:41:59 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,6 +5,7 @@ class Post < ActiveRecord::Base
   has_many :users, through: :comments
 
   accepts_nested_attributes_for :categories
+  accepts_nested_attributes_for :comments
 
   def categories_attributes=(category_attributes)
     category_attributes.values.each do |category_attribute|
